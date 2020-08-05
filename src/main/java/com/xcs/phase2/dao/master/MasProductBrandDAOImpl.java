25aa4d9fc379e7b692049d8462a9f0fb2bd6775e
nanyshiro <bakugan_chun@hotmail.com>
2020-08-05 18:04:58 +0700

    (change) เพิ่ม Query #MasProductBrandinsAll (change) เพิ่ม Query #MasProductBrandupdAll (change) เพิ่ม Query #MasProductBrandupdDelete

--- src/main/java/com/xcs/phase2/dao/master/MasProductBrandDAOImpl.java
+++ src/main/java/com/xcs/phase2/dao/master/MasProductBrandDAOImpl.java
@@ -1,10 +1,12 @@
 package com.xcs.phase2.dao.master;
 
 
+import com.xcs.phase2.constant.Message;
 import com.xcs.phase2.model.master.MasProductBrand;
 import com.xcs.phase2.request.master.MasProductBrandgetByConReq;
 import com.xcs.phase2.request.master.MasProductBrandupdDeleteReq;
 import com.xcs.phase2.response.master.MasProductBrandinsAllResponse;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.RowMapper;
@@ -85,16 +87,85 @@ public class MasProductBrandDAOImpl extends MasterExt implements MasProductBrand
 
     @Override
     public MasProductBrandinsAllResponse MasProductBrandinsAll(MasProductBrand req) {
-        return null;
+    	
+    	MasProductBrandinsAllResponse res = new MasProductBrandinsAllResponse();
+    	
+    	try {
+    		String PRODUCT_BRAND_ID = getSequences("SELECT MAS_PRODUCT_BRAND_SEQ.NEXTVAL FROM DUAL");
+    		
+    		log.info("[getSequences] PRODUCT_GROUP_ID : "+PRODUCT_BRAND_ID);
+    		
+    		StringBuilder sqlBuilder = new StringBuilder()
+			    .append("Insert into MAS_PRODUCT_BRAND (" +
+			    		"PRODUCT_BRAND_ID," +
+			    		"PRODUCT_BRAND_CODE," +
+			    		"PRODUCT_BRAND_NAME_TH," +
+			    		"PRODUCT_BRAND_NAME_EN," +
+			    		"IS_ACTIVE," +
+			    		"PRODUCT_GROUP_CODE," +
+			    		"PRODUCT_CATEGORY_CODE) " +
+			    		"values (" +
+			    		"'"+PRODUCT_BRAND_ID+"'," +
+	    	    		"'"+req.getPRODUCT_BRAND_CODE()+"'," +
+	    	    		"'"+req.getPRODUCT_BRAND_NAME_TH()+"'," +
+			    		"'"+req.getPRODUCT_BRAND_NAME_EN()+"'," +
+			    		"'"+req.getIS_ACTIVE()+"'," +
+			    		"'"+req.getPRODUCT_GROUP_CODE()+"'," +
+			    		"'"+req.getPRODUCT_CATEGORY_CODE()+"' )");
+    		
+    		
+				log.info("[SQL MasProductBrandinsAll] : "+sqlBuilder.toString());
+				
+				getJdbcTemplate().update(sqlBuilder.toString(), new Object[]{});
+	            res.setPRODUCT_BRAND_ID(Integer.parseInt(PRODUCT_BRAND_ID));
+
+	            res.setIsSuccess(Message.TRUE);
+	            res.setMsg(Message.COMPLETE);
+
+	            return res;
+    		
+    	} catch (Exception e){
+    		e.printStackTrace();
+            res.setIsSuccess(Message.FALSE);
+            res.setMsg(e.getMessage());
+            res.setPRODUCT_BRAND_ID(0);
+            return res;		
+    	}
+
     }
 
     @Override
     public Boolean MasProductBrandupdAll(MasProductBrand req) {
-        return null;
+    	
+    	try { 		
+    		StringBuilder sqlBuilderSub = new StringBuilder()
+			    .append("UPDATE MAS_PRODUCT_BRAND SET "
+			    		+"PRODUCT_BRAND_CODE = '"+req.getPRODUCT_BRAND_CODE()+"', " 
+			    		+"PRODUCT_BRAND_NAME_TH = '"+req.getPRODUCT_BRAND_NAME_TH()+"', "
+			    		+"PRODUCT_BRAND_NAME_EN = '"+req.getPRODUCT_BRAND_NAME_EN()+"', "
+			    		+"IS_ACTIVE = '" +req.getIS_ACTIVE()+"',"
+			    		+"PRODUCT_GROUP_CODE = '" +req.getPRODUCT_GROUP_CODE()+"', "
+			    		+"PRODUCT_CATEGORY_CODE = '" +req.getPRODUCT_CATEGORY_CODE()+"' "
+			    		+"WHERE PRODUCT_BRAND_ID = '"+req.getPRODUCT_BRAND_ID()+"'");
+
+				log.info("[SQL MasProductBrandupdAll] : "+sqlBuilderSub.toString());
+				
+				getJdbcTemplate().update(sqlBuilderSub.toString(), new Object[]{});
+	            return true;
+    		
+    	} catch (Exception e){
+    		e.printStackTrace();
+            return false;		
+    	}
     }
 
     @Override
     public Boolean MasProductBrandupdDelete(MasProductBrandupdDeleteReq req) {
-        return null;
+
+    	StringBuilder sqlBuilder = new StringBuilder().append("UPDATE MAS_PRODUCT_BRAND SET IS_ACTIVE = '0' WHERE PRODUCT_BRAND_ID = '" + req.getPRODUCT_BRAND_ID() + "' ");	
+        
+    	getJdbcTemplate().update(sqlBuilder.toString(), new Object[]{});
+        return true;
+    	
     }
 }
