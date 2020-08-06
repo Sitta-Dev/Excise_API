9aa61c09d537b2e041e6b0322b026c2f24c92a9d
nanyshiro <bakugan_chun@hotmail.com>
2020-08-06 16:44:20 +0700

    (change) เพิ่ม Query & Model & Request #MasProductTypegetByCon

--- src/main/java/com/xcs/phase2/request/master/MasProductTypegetByConReq.java
+++ src/main/java/com/xcs/phase2/request/master/MasProductTypegetByConReq.java
@@ -8,6 +8,7 @@ public class MasProductTypegetByConReq extends MasterRequest {
     private String TEXT_SEARCH;
     private int PRODUCT_CATEGORY_ID;
     private int PRODUCT_TYPE_ID;
+    private String PRODUCT_TYPE_CODE;
 
 
 
