03c8db57bb7fb8113dcecbae207a078cbbcb4436
Sitta-Dev <sitta.suk@gmail.com>
2020-08-06 16:52:14 +0700

    change query & map data @MasProductMappinggetByConAdv

--- src/main/java/com/xcs/phase2/request/master/MasProductMappinggetByConAdvReq.java
+++ src/main/java/com/xcs/phase2/request/master/MasProductMappinggetByConAdvReq.java
@@ -8,5 +8,12 @@ public class MasProductMappinggetByConAdvReq extends MasterRequest {
     private String PRODUCT_GROUP_ID;
     private String PRODUCT_CODE;
     private String PRODUCT_NAME_DESC;
-
+    
+    private String PRODUCT_CATEGORY_ID;
+    private String PRODUCT_TYPE_ID;
+    private String SIZES;
+    private String SIZES_UNIT;
+    private String PRODUCT_BRAND_NAME;
+    private String PRODUCT_SUBBRAND_NAME;
+    private String PRODUCT_MODEL_NAME;
 }
