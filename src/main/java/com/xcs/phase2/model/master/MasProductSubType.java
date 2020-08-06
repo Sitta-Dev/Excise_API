c66220781f4395ca840e8bbaf096aa9cd98f02f3
nanyshiro <bakugan_chun@hotmail.com>
2020-08-06 17:30:49 +0700

    (change) เพิ่ม Query & Model & Request #MasProductSubTypegetByCon

--- src/main/java/com/xcs/phase2/model/master/MasProductSubType.java
+++ src/main/java/com/xcs/phase2/model/master/MasProductSubType.java
@@ -4,4 +4,11 @@ import lombok.Data;
 
 @Data
 public class MasProductSubType extends MasterProductModel {
+	private int PRODUCT_SUBTYPE_ID;
+	private int PRODUCT_TYPE_ID;
+	private String PRODUCT_SUBTYPE_CODE;
+	private String PRODUCT_SUBTYPE_NAME;
+	private int IS_TAX;
+	private int IS_ACTIVE;
+	
 }
--- src/main/java/com/xcs/phase2/request/master/MasProductSubTypegetByConReq.java
+++ src/main/java/com/xcs/phase2/request/master/MasProductSubTypegetByConReq.java
@@ -8,7 +8,8 @@ public class MasProductSubTypegetByConReq extends MasterRequest {
     private String TEXT_SEARCH;
     private int PRODUCT_SUBTYPE_ID;
     private int PRODUCT_TYPE_ID;
-
+    private String PRODUCT_SUBTYPE_CODE;
+    
 
 
 }
